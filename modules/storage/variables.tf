b0867177c1986ddd5d06565a4745c41db27fe94e
SundaramDiraviam <sundarsubramani94@gmail.com>
2026-05-07 17:03:06 +0530

    feat(storage): add variables

---
+++ modules/storage/variables.tf
@@ -0,0 +1,3 @@
+variable "project"     { description = "Project name prefix"; type = string }
+variable "environment" { description = "Environment name"; type = string }
+variable "aws_region"  { description = "AWS region"; type = string }
